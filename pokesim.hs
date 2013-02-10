492ed4533661fd2e64edc460ed8211c7ed3cfb2f
Mary Ontiveros <mary@HP-Pavilion-dv7-Notebook-PC.(none)>
2013-02-10 10:36:42 -0430

    IO

--- pokesim.hs
+++ pokesim.hs
@@ -1,7 +1,30 @@
-module Main where
+module Main ( main ) where
 
+import System.IO
+import Data.Char(toUpper)
+import System.Posix.Files
 import System.Environment (getArgs)
 
+
+
+-- Funcion que abre los archivos de entrada
+abrirFile :: [String] -> IO ()
+
+abrirFile args = do 
+  fileEsp  <- readFile ((!!) args 0) 
+  fileAtaq <- readFile ((!!) args 1) 
+  equipo1  <- readFile ((!!) args 2) 
+  equipo2  <- readFile ((!!) args 3) 
+  putStr (return fileEsp)
+  
+-- Funcion principal 
 main :: IO ()
 
-main = print $ "hola"
+main = do 
+  args  <- getArgs
+  if (length args) == 4 then
+    abrirFile(args)
+  else 
+    print "Tiene que introducir 4 nombres de archivos"
+  
+
