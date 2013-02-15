05d7852bc3586da5b3231f53bb32876acf0f99e2
Mary Ontiveros <mary@HP-Pavilion-dv7-Notebook-PC.(none)>
2013-02-15 18:19:35 -0430

    diccionarios

--- pokesim.hs
+++ pokesim.hs
@@ -2,10 +2,12 @@ module Main ( main ) where
 
 import Pokemon
 import System.IO
+import Data.Maybe
 import Data.Char(toUpper)
 import Data.List.Split
 import System.Posix.Files
 import System.Environment (getArgs)
+import qualified Data.Map as Dic
 
 
 -- Funcion que crea una lista de especies ---
@@ -21,56 +23,63 @@ listaEspecie (x:xs) = crear x : listaEspecie xs
 	
 -- Funcion que crea una lista de ataques    
 listaAtaque :: [[String]] -> [Ataque]   
-listaAtaque [] = []
-listaAtaque (x:xs) = crear x : listaAtaque xs 
+listaAtaque = map crear  
   where
     crear [c0,c1,c2,c3,c4] = 
       Ataque c0 (read c1 :: Tipo) (read c2  :: Bool) 
 		(read c3  :: Int) (read c4  :: Int) 
 
 		
--- Funcion que crea una lista de monstruo		   
+-- Funcion que crea una lista de monstruo por equipo participante	   
 listaMonstruo :: [[String]] -> [Especie] -> [Ataque] -> [Monstruo]   
 listaMonstruo [] esp atq = []
-listaMonstruo (x:xs) esp atq = crear x : listaMonstruo xs esp atq
+listaMonstruo (x:xs) esp atq = case crear x of
+  Nothing -> listaMonstruo xs esp atq
+  Just x  -> x : listaMonstruo xs esp atq
   where
-    crear [c0,c1,c2,c3,c4,c5,c6] = Monstruo (getEspecie esp (read c0 :: Int)) c1 (read c2  :: Int) 0 
-	  [getAtaque atq c3, getAtaque atq c4, getAtaque atq c5, getAtaque atq c6] 35 255		
-		
-		
--- Funcion que me devuelve una Especie	
-getEspecie ::[Especie] -> Int -> Especie
-getEspecie esp num = (!!) (filter (\x -> numero x == num) esp) 0 
-  
-  
--- Funcion que me devuelve un Ataque	
-getAtaque ::[Ataque] -> String -> Ataque
-getAtaque atq num = (!!) (filter (\x -> nombreAtaq x == num) atq) 0 
+    crear [c0,c1,c2,c3,c4,c5,c6] = Monstruo esps c1 (read c2  :: Int) 0 atqs 35 255
+	  where
+	    esps = Dic.lookup (read c0 :: Int) esp
+	    atqs  = catMaybes (map (\ c -> Dic.lookup c atq) [c3, c4, c5, c6])
+	    
 
-		   
--- Funcion que parsea los archivos 
-parser :: [String] -> [[[String]]]
-parser [] = []
-parser (x:xs) = separarComas (splitOn "\n" x) : parser xs
+	    
+-- Diccionario de Especies 
+diccEspecie :: [Especie] -> Dic.Map Int Especie
+diccEspecie  = foldl (\ mapa esp -> Dic.insert (numero esp) esp mapa) Dic.empty 
 
-  where 
-    separarComas [] = []
-    separarComas (x:xs) = splitOn "," x : separarComas xs 
- 
+
+-- Diccionario de Ataques
+diccAtaque :: [Ataque] -> Dic.Map String Ataque
+diccAtaque  = foldl (\ mapa ata -> Dic.insert (nombreAtaq ata) ata  mapa) Dic.empty 
+
+
+-- Funcion que parsea los archivos  
+parser :: String -> [[String]]
+parser x = map (splitOn ",") (lines x)
+  
  
 -- Funcion que abre los archivos de entrada
 abrirFile :: [String] -> IO ()
-abrirFile args = do 
-  fileEsp  <- readFile ((!!) args 0) 
-  fileAtaq <- readFile ((!!) args 1) 
-  equipo1  <- readFile ((!!) args 2) 
-  equipo2  <- readFile ((!!) args 3) 
-  let parseado = parser [fileEsp, fileAtaq, equipo1, equipo2]
-  let esp = listaEspecie ((!!) parseado 0)
-  let atq = listaAtaque ((!!) parseado 1)
-  let equipo1 = listaMonstruo ((!!) parseado 2) esp atq
-  let equipo2 = listaMonstruo ((!!) parseado 3) esp atq
-  print esp
+abrirFile [fileEspS,fileAtaqS,equipo1S,equipo2S] = do 
+  fileEsp  <- readFile fileEspS 
+  fileAtaq <- readFile fileAtaqS
+  equipo1  <- readFile equipo1S
+  equipo2  <- readFile equipo2S
+  
+  -- Parseo los archivos de las especies, ataques y equipos
+  let [espe, atque, equip1 , equip2] = map parser [fileEsp, fileAtaq, equipo1, equipo2]
+  
+  -- Se define un diccionario de Especies
+  let esp = diccEspecie (listaEspecie espe)
+  
+  -- Se define un diccionario de ataques
+  let atq = diccAtaque (listaAtaque atque)
+  
+  -- Se Crean los equipos participantes
+  let equipo1 = listaMonstruo (equip1) esp atq
+  let equipo2 = listaMonstruo (equip2) esp atq
+  print "Bienvenido a una Nueva Batalla Pokemon"
   
   
 -- Funcion principal 
